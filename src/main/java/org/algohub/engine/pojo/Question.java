e6383873e7c1b53bbde341671eb97fe5774bc839
soulmachine <soulmachine@gmail.com>
2017-03-12 22:07:06 -0700

    Renamed field level to difficulty and removed the score field

--- src/main/java/org/algohub/engine/pojo/Question.java
+++ src/main/java/org/algohub/engine/pojo/Question.java
@@ -21,8 +21,7 @@ public class Question {
   private final Map<String, String> description;
   private final String category;
   private final String[] tags;
-  private final int level;
-  private final int score;
+  private final int difficulty;
   @JsonProperty("time_limit") private final int timeLimit;
   @JsonProperty("memory_limit") private final int memoryLimit;
   @JsonProperty("related_problems") private final String[] relatedProblems;
@@ -40,8 +39,7 @@ public class Question {
       @JsonProperty("description") final Map<String, String> description,
       @JsonProperty("category") final String category,
       @JsonProperty("tags") final String[] tags,
-      @JsonProperty("level") final int level,
-      @JsonProperty("score") final int score,
+      @JsonProperty("difficulty") final int difficulty,
       @JsonProperty("time_limit") final int timeLimit,
       @JsonProperty("memory_limit") final int memoryLimit,
       @JsonProperty("related_problems") final String[] relatedProblems,
@@ -54,8 +52,7 @@ public class Question {
     this.description = description;
     this.category = category;
     this.tags = tags;
-    this.level = level;
-    this.score = score;
+    this.difficulty = difficulty;
     this.timeLimit = timeLimit;
     this.memoryLimit = memoryLimit;
     this.relatedProblems = relatedProblems;
@@ -107,12 +104,8 @@ public class Question {
     return tags;
   }
 
-  public int getLevel() {
-    return level;
-  }
-
-  public int getScore() {
-    return score;
+  public int getDifficulty() {
+    return difficulty;
   }
 
   public int getTimeLimit() {
