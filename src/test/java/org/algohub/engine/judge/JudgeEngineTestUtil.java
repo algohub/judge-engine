a22116b4524a8321d1b325db2075d5142d083b0b
soulmachine <soulmachine@gmail.com>
2017-03-16 16:18:30 -0700

    Used FilenameFilter

--- src/test/java/org/algohub/engine/judge/JudgeEngineTestUtil.java
+++ src/test/java/org/algohub/engine/judge/JudgeEngineTestUtil.java
@@ -13,8 +13,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -25,20 +23,17 @@ final class JudgeEngineTestUtil {
   static void batchJudge(final LanguageType languageType) {
     final File rootDir = new File("src/test/resources/solutions/");
     final File problemDir = new File("src/test/resources/problems/");
-    final Pattern pattern = Pattern.compile("\\w+/\\." + languageType.getFileSuffix());
 
     try {
       for (final File solutionDir : rootDir.listFiles()) {
+        final String problemId = solutionDir.getName();
         final String problemJson = new String(java.nio.file.Files.readAllBytes(
-            Paths.get(problemDir.getAbsolutePath(), solutionDir.getName() + ".json")),
+            Paths.get(problemDir.getAbsolutePath(), problemId + ".json")),
             StandardCharsets.UTF_8);
-        final Problem problem = ObjectMapperInstance.INSTANCE.readValue(problemJson,
-            Problem.class);
+        final Problem problem = ObjectMapperInstance.INSTANCE.readValue(problemJson, Problem.class);
 
-        for (final File solutionFile : solutionDir.listFiles()) {
-          final Matcher matcher = pattern.matcher(solutionFile.getName());
-          if (!matcher.matches()) continue;
-
+        for (final File solutionFile : solutionDir.listFiles(
+            (dir, name) -> name.toLowerCase().endsWith("." + languageType.getFileSuffix()))) {
           final String userCode =
               new String(java.nio.file.Files.readAllBytes(solutionFile.toPath()),
                   StandardCharsets.UTF_8);
@@ -51,7 +46,7 @@ final class JudgeEngineTestUtil {
     }
   }
 
-  private static void judgeOne(final Problem problem, final String userCode,
+  static void judgeOne(final Problem problem, final String userCode,
       LanguageType languageType) {
     final JudgeResult result = JUDGE_ENGINE.judge(problem, userCode, languageType);
     if(StatusCode.ACCEPTED != result.getStatusCode()) {
