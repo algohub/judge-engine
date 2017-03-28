dae9d73164ac717ff97ebfca5ef9ea3e7a612eb4
soulmachine <soulmachine@gmail.com>
2017-03-28 03:25:24 -0700

    Refactored install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,18 @@ if [[ ! -d "/usr/local/include/rapidjson" ]]; then
   rm -rf ./rapidjson
 fi
 
-sudo cp src/main/resources/cpp/* /usr/local/include/
-sudo cp -r src/main/resources/python/algohub/  /usr/local/lib/python3.5/dist-packages/
-sudo mkdir -p /usr/local/lib/site_ruby/2.3.0
-sudo cp src/main/resources/ruby/algohub.rb /usr/local/lib/site_ruby/2.3.0/
+git clone https://github.com/algohub/judge-engine-languages-support.git /tmp/judge-engine-languages-support
+
+if [[ ! -d "/usr/local/lib/python3.5/dist-packages/algohub/" ]]; then
+  sudo cp -r /tmp/judge-engine-languages-support/judge-engine-python-support/algohub/ /usr/local/lib/python3.5/dist-packages/
+fi
+
+if [[ ! -f "/usr/local/lib/site_ruby/2.3.0/algohub.rb" ]]; then
+  sudo mkdir -p /usr/local/lib/site_ruby/2.3.0
+  sudo cp /tmp/judge-engine-languages-support/ruby/algohub.rb /usr/local/lib/site_ruby/2.3.0/
+fi
+
+command -v mvn >/dev/null 2>&1 || { echo >&2 "Maven is no installed, now start to install..."; sudo apt -y --no-install-recommends install maven; }
 
 mvn clean package
 
